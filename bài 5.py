862c73da79050a36ff5c73caec7ed9610d06b6df
thanhngan2002 <73688091+thanhngan2002@users.noreply.github.com>
2020-11-21 15:45:01 +0700

    Add files via upload

---
+++ bài 5.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 14:47:34 2020
+
+@author: Admin
+"""
+
+import math
+list1=[10, 234, 456, 124, 136, 158]
+i=0
+for i in range(len(list1)):
+    print(list1[i])
+print("kết thúc chương trình 1")
+i=0
+for i in range(len(list1)):
+    print(list1[i])
+    print("logarit là:",math.log10(list1[i]))
+    print("kết thúc chương trình 2")
+
+    
