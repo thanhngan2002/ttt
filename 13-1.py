f5c7816b92f59be9d5b4039e97a7a1e7ea94ee99
thanhngan2002 <73688091+thanhngan2002@users.noreply.github.com>
2020-12-05 14:19:02 +0700

    Spyder

---
+++ 13-1.py
@@ -0,0 +1,19 @@
+
+import os 
+
+for (r, d, f) in os.walk('C:'):
+    print("thư mục gốc và thư mục trong ổ đĩa C:",r)
+    print("các thư mục con từ các thư mục gôc",d)
+    print("các tập tin từ thư mục gốc và thư mục",f)
+
+for (r, d, f) in os.walk('C:'):
+    list1 = f
+    list2 = r
+
+
+
+
+
+        
+    
+    
