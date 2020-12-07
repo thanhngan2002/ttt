3b277d5102cd3d056b87a4d4aa5af967a7a4efa5
thanhngan2002 <73688091+thanhngan2002@users.noreply.github.com>
2020-12-07 12:03:24 +0700

    Update 13-1.py

--- 13-1.py
+++ 13-1.py
@@ -1,12 +1,14 @@
 
 import os 
-
-for (r, d, f) in os.walk('C:'):
+path = 'C:'
+f = list()
+r = list()
+for (r, d, f) in os.walk(path):
     print("thư mục gốc và thư mục trong ổ đĩa C:",r)
     print("các thư mục con từ các thư mục gôc",d)
     print("các tập tin từ thư mục gốc và thư mục",f)
 
-for (r, d, f) in os.walk('C:'):
+for (r, d, f) in os.walk(path):
     list1 = f
     list2 = r
 
