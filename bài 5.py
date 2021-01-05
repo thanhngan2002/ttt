cf737f6eff91e4612dc527eedd1262230a7364c0
thanhngan2002 <73688091+thanhngan2002@users.noreply.github.com>
2021-01-05 14:33:10 +0700

    Update bài 5.py

--- bài 5.py
+++ bài 5.py
@@ -6,7 +6,7 @@ Created on Fri Nov 20 14:47:34 2020
 """
 
 import math
-list1=[10, 234, 456, 124, 136, 158]
+list1=[10.0, 234.0, 456.1, 124.2, 136.3, 158.5]
 i=0
 for i in range(len(list1)):
     print(list1[i])
