32777e7c3f3bee299abc5992571574073b7cf7f8
thanhngan2002 <73688091+thanhngan2002@users.noreply.github.com>
2021-01-06 15:28:25 +0700

    Update 13-1.py

--- 13-1.py
+++ 13-1.py
@@ -3,17 +3,15 @@ import os
 path = 'C:'
 list1=[]
 list2=[]
-for (r, d, f) in os.walk(path):
-    print("thư mục gốc và thư mục trong ổ đĩa C:",r)
-    print("các thư mục con từ các thư mục gôc",d)
-    print("các tập tin từ thư mục gốc và thư mục",f)
 
-for (r, d, f) in os.walk(path):
-    list1.append(f)
-    list2.append(d)
+print(os.listdir(path))
+print("các thư mục:")
+list1 = next(os.walk(path))[1]
 print(list1)
-print(list2)
 
+print("các tệp:")
+list2 = next(os.walk(path))[2]
+print(list2)
 
 
 
