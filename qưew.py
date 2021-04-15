f63b6ad419dd1bcab5c748fe45a41b4d754503d4
thanhngan2002 <73688091+thanhngan2002@users.noreply.github.com>
2021-04-15 07:37:24 +0700

    Add files via upload

---
+++ qưew.py
@@ -0,0 +1,10 @@
+from anytree import Node, RenderTree
+udo = Node("Udo")
+marc = Node("Marc", parent=udo)
+lian = Node("Lian", parent=marc)
+dan = Node("Dan", parent=udo)
+jet = Node("Jet", parent=dan)
+jan = Node("Jan", parent=dan)
+joe = Node("Joe", parent=dan)
+for pre, fill, node in RenderTree(udo):
+     print("%s%s" % (pre, node.name))
